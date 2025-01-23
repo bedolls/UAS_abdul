0be25442ed109f7bb2b10fed012bcd5c1424d257
A <147518138+bedolls@users.noreply.github.com>
2025-01-23 22:27:33 +0700

    ubah nma database

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $username = "root";
 $password =  "";
-$database = "project_is62";
+$database = "tbl_buku";
 
 $koneksi = mysqli_connect($host,$username,$password,$database);
 
